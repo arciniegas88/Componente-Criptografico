91a56baf903a11ea66bb277cb6f743e7fc417378
GustavoAdolfo <arciniegas88@gmail.com>
2015-10-22 15:52:02 -0500

    este es un cambio desde el trunk

--- Security-Module/src/main/java/co/com/javeriana/security/messages/SecureMessage.java
+++ Security-Module/src/main/java/co/com/javeriana/security/messages/SecureMessage.java
@@ -6,6 +6,7 @@ import javax.xml.bind.annotation.XmlRootElement;
  * Created by garciniegas on 18/10/2015.
  
  cambio por defecto 1100 final
+ $esta es una linea que estorva por conflictos
  */
 
 @XmlRootElement(namespace = "http://javeriana.com/aes/secutiry/1.0.0")
