95ee70635daea4e485ee4eebbcf6b9611b3f35e2
GustavoAdolfo <arciniegas88@gmail.com>
2015-10-22 15:48:17 -0500

    ajuste para merge cuidadoso

--- Security-Module/src/main/java/co/com/javeriana/security/messages/SecureMessage.java
+++ Security-Module/src/main/java/co/com/javeriana/security/messages/SecureMessage.java
@@ -5,7 +5,8 @@ import javax.xml.bind.annotation.XmlRootElement;
 /**
  * Created by garciniegas on 18/10/2015.
  
- cambio por defecto 1100 final
+ cambio por defecto 1100 final - ESTE CAMBIO
+ DEBE SER MANEJADO POR MERGER ESTRATEGICO
  */
 
 @XmlRootElement(namespace = "http://javeriana.com/aes/secutiry/1.0.0")
